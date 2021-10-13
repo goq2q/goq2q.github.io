a3c5e8279a771ca708749291e6efcf0664069e77
John Wostenberg <jwosty@users.noreply.github.com>
2021-10-12 23:01:45 -0600

    Build site from 3fb81b484215724db0106e53cf8167ee02774d3a

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,5 +1,5 @@
 var store = [{
-        "title": "Using ASCII Waveforms to test real-time audio code",
+        "title": "Using ASCII waveforms to test real-time audio code",
         "excerpt":"I draw sound wave ASCII art in Q2Q’s source code. These ASCII art waveforms ensure that the real-time audio engine at the heart of Q2Q stays bug-free. Software development best-practices dictate that if you want your software to be high-quality (who doesn’t?), you test your code, you test it automatically,...","categories": ["tech"],
         "tags": [],
         "url": "/blog/tech/using-ascii-waveforms-to-test-real-time-audio-code",
